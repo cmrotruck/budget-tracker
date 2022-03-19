08b8246b13d3c810ef13d1ca4c4f2378a36c66b1
cmrotruck <crotruck@gmail.com>
2022-03-19 13:50:50 -0400

    added manifest so app can be installed.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 module.exports = {
   entry: { index: "./public/js/index.js", idb: "./public/js/idb.js" },
   output: {
     filename: "[name].bundle.js",
-    path: __dirname + "/public/dist",
+    path: __dirname + "/public",
   },
   module: {
     rules: [
@@ -30,5 +31,24 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new WebpackPwaManifest({
+      name: "Budget Tracker",
+      short_name: "Budget_Tracker",
+      description:
+        "An app that allows you to enter debits and credits to track a budget.",
+      icons: [
+        {
+          src: path.resolve("public/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: "icons",
+        },
+      ],
+      theme_color: "#000000",
+      background_color: "#ffffff",
+      start_url: "./index.html",
+      display: "standalone",
+    }),
+  ],
   mode: "development",
 };
